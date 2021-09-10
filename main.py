312c1beb335f58ab3d33d49af674b5639ac3ef82
dev-gvs <44653764+dev-gvs@users.noreply.github.com>
2021-09-10 23:04:20 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+def main():
+    try:
+        a = int(input("Введите A: "))
+        b = int(input("Введите B: "))
+        x = int(input("Введите X: "))
+        if x <= 4:
+            y = ((a * a) / (x * x)) + 6 * x
+        else:
+            y = (b * b) * ((4 + x) * (4 + x))
+        print(f"y = {y:.2f}")
+    except ValueError:
+        print("Неверные входные данные!")
+    except (OverflowError, ZeroDivisionError):
+        print("Нет решения!")
+
+
+if __name__ == "__main__":
+    main()
+    input("Нажмите любую клавишу чтобы выйти...")
